bb8b05e47278549e508886a7c2aa602eafbaef00
Sam Waseda <o.waseda@mpie.de>
2021-11-04 22:12:49 +0100

    refactoring of computePot

--- dft/main.py
+++ dft/main.py
@@ -14,6 +14,8 @@ class Hamiltonian:
         self.Znuc = Znuc
         self.Nel = Znuc
         self._rhonuc_G = None
+        self._rho = None
+        self._rho_G = None
 
     @property
     def rhonuc_G(self):
@@ -58,7 +60,6 @@ class Hamiltonian:
 
     @property
     def hamMat(self):
-        # kinetic energy
         return np.fft.ifft(self.veff)[self._G_indices] + 0.5 * self.Gvec**2 * np.eye(self.Npw)
 
     def computeRho(self, vals, vecs, Nel, kT):
@@ -90,27 +91,63 @@ class Hamiltonian:
                             )
         return rho
 
-    def computePot(self, rho):
-        rho_G = np.fft.ifft(rho)
-        rho_G += self.rhonuc_G
-        rho_G[0]=0.
-        V_G = rho_G * self.coulomb
-        vH = np.real (np.fft.fft(V_G))
-        self.E_H = 0.5 * np.vdot(rho_G, V_G).real * self.L
-        #self.E_H = 0.5 * sum(np.fft.fft(rho_G).real * vH) * self.dL_2
+    @property
+    def rho(self):
+        return self._rho
 
-        # xc contribution via x-alpha
-        alpha = -0.73855877 # -3/4 (3/pi)**(1/3)
-        self.eXc = sum(r**(4./3.) for r in rho if r > 0) \
-                 * alpha * self.dL_2
-        vXc = (4./3.) * alpha * np.asarray ([r**(1./3.) if r > 0 else 0 for r in rho])
-        self.veff = vH + vXc
-        # External local potential
-        if any(f == 'v_loc' for f in self.__dir__()):
-            self.veff += self.v_loc
-            self.eLoc = sum (self.v_loc * rho) * self.dL_2
+    @rho.setter
+    def rho(self, rho_in):
+        self._rho = np.asarray(rho_in)
+        self._rho_G = None
+
+    @property
+    def rho_G(self):
+        if self._rho_G is None:
+            self._rho_G = np.fft.ifft(self.rho)
+            self._rho_G += self.rhonuc_G
+            self._rho_G[0] = 0.
+        return self._rho_G
+
+    @property
+    def alpha(self):
+        return -3/4 * (3/np.pi)**(1/3)
+
+    @property
+    def eXc(self):
+        return np.sum(self.rho[self.rho > 0]**(4./3.)) * self.alpha * self.dL_2
+
+    @property
+    def vXc(self):
+        return (4./3.) * self.alpha * np.maximum(self.rho, 0)**(1./3.)
+
+    @property
+    def V_G(self):
+        return self.rho_G * self.coulomb
+
+    @property
+    def vH(self):
+        return np.real (np.fft.fft(self.V_G))
+
+    @property
+    def veff(self):
+        veff = self.vH + self.vXc
+        if 'v_loc' in self.__dir__():
+            veff += self.v_loc
+        return veff
+
+    @property
+    def eLoc(self):
+        if 'v_loc' in self.__dir__():
+            return sum (self.v_loc * self.rho) * self.dL_2
         else:
-            self.eLoc = 0.
+            return 0.
+
+    @property
+    def E_H(self):
+        return 0.5 * np.vdot(self.rho_G, self.V_G).real * self.L
+
+    def computePot(self, rho):
+        self.rho = rho
 
     def getEnergy (self):
         return self.Ekin + self.eXc + self.E_H + self.eLoc
